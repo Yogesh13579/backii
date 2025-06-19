c8e9b1aaa75d4ab2003778a0b2f1c91939cedd8e
Yogesh13579 <yogeshk0051@gmail.com>
2025-06-19 16:40:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+console.log("hiiiii")
+const express = require('express')
+//import express from "express" (alterna. of line 2)
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/yogi',(req,res)=>{
+    res.send('<h1>yogi baba ki jay</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
